5608ff6bdd71eb049a18c2c9caf53a83abe225e1
Daniel Priori <danpriori@gmail.com>
2024-01-10 22:14:44 -0300

    update firewall rules

--- SignallingWebServer/platform_scripts/cmd/ss_startup.ps1
+++ SignallingWebServer/platform_scripts/cmd/ss_startup.ps1
@@ -1,42 +1,42 @@
 #Set up firewall
 Write-Host "Setting Firewall rules"
-# For Watchmaker Unreal Engine plugin in builds where it is named WatchmakerFakeApp.exe
-if(!(New-NetFirewallRule -Name "Allow WatchmakerFake-App Binary WebRTC in" -DisplayName "Allow WatchmakerFake-App Binary WebRTC in" -Action "Allow" -Direction "Inbound" -Protocol "udp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\WatchmakerFakeApp\Binaries\Win64\WatchmakerFakeApp.exe" -ErrorAction Ignore)){
-    Set-NetFirewallRule -Name "Allow WatchmakerFake-App Binary WebRTC in" -Action "Allow" -Direction "Inbound" -Protocol "udp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\WatchmakerFakeApp\Binaries\Win64\WatchmakerFakeApp.exe"
+# For Watchmaker Unreal Engine plugin in builds where it is named Watch_France.exe
+if(!(New-NetFirewallRule -Name "Allow Watch_France-App Binary WebRTC in" -DisplayName "Allow Watch_France Binary WebRTC in" -Action "Allow" -Direction "Inbound" -Protocol "udp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\Watch_France\Binaries\Win64\Watch_France.exe" -ErrorAction Ignore)){
+    Set-NetFirewallRule -Name "Allow Watch_France Binary WebRTC in" -Action "Allow" -Direction "Inbound" -Protocol "udp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\Watch_France\Binaries\Win64\Watch_France.exe"
 }
-if(!(New-NetFirewallRule -Name "Allow WatchmakerFake-App Binary WebRTC out" -DisplayName "Allow WatchmakerFake-App WebRTC Binary WebRTC out" -Action "Allow" -Direction "Outbound" -Protocol "udp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\WatchmakerFakeApp\Binaries\Win64\WatchmakerFakeApp.exe" -ErrorAction Ignore)){
-    Set-NetFirewallRule -Name "Allow WatchmakerFake-App Binary WebRTC out" -Action "Allow" -Direction "Outbound" -Protocol "udp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\WatchmakerFakeApp\Binaries\Win64\WatchmakerFakeApp.exe"
+if(!(New-NetFirewallRule -Name "Allow Watch_France Binary WebRTC out" -DisplayName "Allow Watch_France WebRTC Binary WebRTC out" -Action "Allow" -Direction "Outbound" -Protocol "udp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\Watch_France\Binaries\Win64\Watch_France.exe" -ErrorAction Ignore)){
+    Set-NetFirewallRule -Name "Allow Watch_France Binary WebRTC out" -Action "Allow" -Direction "Outbound" -Protocol "udp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\Watch_France\Binaries\Win64\Watch_France.exe"
 }
-# For Watchmaker Unreal Engine plugin in builds where it is named WatchmakerFakeApp.exe
-if(!(New-NetFirewallRule -Name "Allow WatchmakerFake-App WebRTC in" -DisplayName "Allow WatchmakerFake-App WebRTC in" -Action "Allow" -Direction "Inbound" -Protocol "udp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\WatchmakerFakeApp.exe" -ErrorAction Ignore)){
-    Set-NetFirewallRule -Name "Allow WatchmakerFake-App WebRTC in" -Action "Allow" -Direction "Inbound" -Protocol "udp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\WatchmakerFakeApp.exe"
+# For Watchmaker Unreal Engine plugin in builds where it is named Watch_France.exe
+if(!(New-NetFirewallRule -Name "Allow Watch_France WebRTC in" -DisplayName "Allow Watch_France WebRTC in" -Action "Allow" -Direction "Inbound" -Protocol "udp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\Watch_France.exe" -ErrorAction Ignore)){
+    Set-NetFirewallRule -Name "Allow Watch_France WebRTC in" -Action "Allow" -Direction "Inbound" -Protocol "udp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\Watch_France.exe"
 }
-if(!(New-NetFirewallRule -Name "Allow WatchmakerFake-App WebRTC out" -DisplayName "Allow WatchmakerFake-App WebRTC out" -Action "Allow" -Direction "Outbound" -Protocol "udp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\WatchmakerFakeApp.exe" -ErrorAction Ignore)){
-    Set-NetFirewallRule -Name "Allow WatchmakerFake-App WebRTC out" -Action "Allow" -Direction "Outbound" -Protocol "udp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\WatchmakerFakeApp.exe"
+if(!(New-NetFirewallRule -Name "Allow Watch_France WebRTC out" -DisplayName "Allow Watch_France WebRTC out" -Action "Allow" -Direction "Outbound" -Protocol "udp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\Watch_France.exe" -ErrorAction Ignore)){
+    Set-NetFirewallRule -Name "Allow Watch_France WebRTC out" -Action "Allow" -Direction "Outbound" -Protocol "udp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\Watch_France.exe"
 }
 # For Watchmaker.exe loader
-if(!(New-NetFirewallRule -Name "Allow WatchmakerFake-App TCP" -DisplayName "Allow WatchmakerFake-App TCP" -Action "Allow" -Direction "Inbound" -Protocol "tcp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\WatchmakerFakeApp.exe" -ErrorAction Ignore)){
-    Set-NetFirewallRule -Name "Allow WatchmakerFake-App TCP" -Action "Allow" -Direction "Inbound" -Protocol "tcp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\WatchmakerFakeApp.exe"
+if(!(New-NetFirewallRule -Name "Allow Watch_France TCP" -DisplayName "Allow Watch_France TCP" -Action "Allow" -Direction "Inbound" -Protocol "tcp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\Watch_France.exe" -ErrorAction Ignore)){
+    Set-NetFirewallRule -Name "Allow Watch_France TCP" -Action "Allow" -Direction "Inbound" -Protocol "tcp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\Watch_France.exe"
 }
-if(!(New-NetFirewallRule -Name "Allow WatchmakerFake-App TCP out" -DisplayName "Allow WatchmakerFake-App TCP out" -Action "Allow" -Direction "Outbound" -Protocol "tcp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\WatchmakerFakeApp.exe" -ErrorAction Ignore)){
-    Set-NetFirewallRule -Name "Allow WatchmakerFake-App TCP out" -Action "Allow" -Direction "Outbound" -Protocol "tcp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\WatchmakerFakeApp.exe"
-}
-
-
-
-if(!(New-NetFirewallRule -Name "Allow WatchmakerFake-App frontend-streamer in" -DisplayName "Allow WatchmakerFake-App frontend-streamer in" -Action "Allow" -Direction "Inbound" -Protocol "tcp" -LocalPort 9090 -Program "C:\Watchmaker\application\WatchmakerFakeApp.exe" -ErrorAction Ignore)){
-    Set-NetFirewallRule -Name "Allow WatchmakerFake-App frontend-streamer in" -Action "Allow" -Direction "Inbound" -Protocol "tcp" -LocalPort 9090 -Program "C:\Watchmaker\application\WatchmakerFakeApp.exe"
-}
-if(!(New-NetFirewallRule -Name "Allow WatchmakerFake-App Binary frontend-streamer in" -DisplayName "Allow WatchmakerFake-App frontend-streamer in" -Action "Allow" -Direction "Inbound" -Protocol "tcp" -LocalPort 9090 -Program "C:\Watchmaker\application\WatchmakerFakeApp\Binaries\Win64\WatchmakerFakeApp.exe" -ErrorAction Ignore)){
-    Set-NetFirewallRule -Name "Allow WatchmakerFake-App Binary frontend-streamer in" -Action "Allow" -Direction "Inbound" -Protocol "tcp" -LocalPort 9090 -Program "C:\Watchmaker\application\WatchmakerFakeApp\Binaries\Win64\WatchmakerFakeApp.exe"
+if(!(New-NetFirewallRule -Name "Allow Watch_France TCP out" -DisplayName "Allow Watch_France TCP out" -Action "Allow" -Direction "Outbound" -Protocol "tcp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\Watch_France.exe" -ErrorAction Ignore)){
+    Set-NetFirewallRule -Name "Allow Watch_France TCP out" -Action "Allow" -Direction "Outbound" -Protocol "tcp" -LocalPort 49152-65535 -Program "C:\Watchmaker\application\Watch_France.exe"
 }
 
 
-if(!(New-NetFirewallRule -Name "Allow WatchmakerFake-App frontend-streamer out" -DisplayName "Allow WatchmakerFake-App frontend-streamer out" -Action "Allow" -Direction "Outbound" -Protocol "tcp" -LocalPort 9090 -Program "C:\Watchmaker\application\WatchmakerFakeApp.exe" -ErrorAction Ignore)){
-    Set-NetFirewallRule -Name "Allow WatchmakerFake-App frontend-streamer out" -Action "Allow" -Direction "Outbound" -Protocol "tcp" -LocalPort 9090 -Program "C:\Watchmaker\application\WatchmakerFakeApp.exe"
+
+if(!(New-NetFirewallRule -Name "Allow Watch_France frontend-streamer in" -DisplayName "Allow Watch_France frontend-streamer in" -Action "Allow" -Direction "Inbound" -Protocol "tcp" -LocalPort 9090 -Program "C:\Watchmaker\application\Watch_France.exe" -ErrorAction Ignore)){
+    Set-NetFirewallRule -Name "Allow Watch_France frontend-streamer in" -Action "Allow" -Direction "Inbound" -Protocol "tcp" -LocalPort 9090 -Program "C:\Watchmaker\application\Watch_France.exe"
 }
-if(!(New-NetFirewallRule -Name "Allow WatchmakerFake-App Binary frontend-streamer out" -DisplayName "Allow WatchmakerFake-App frontend-streamer out" -Action "Allow" -Direction "Outbound" -Protocol "tcp" -LocalPort 9090 -Program "C:\Watchmaker\application\WatchmakerFakeApp\Binaries\Win64\WatchmakerFakeApp.exe" -ErrorAction Ignore)){
-    Set-NetFirewallRule -Name "Allow WatchmakerFake-App Binary frontend-streamer out" -Action "Allow" -Direction "Outbound" -Protocol "tcp" -LocalPort 9090 -Program "C:\Watchmaker\application\WatchmakerFakeApp\Binaries\Win64\WatchmakerFakeApp.exe"
+if(!(New-NetFirewallRule -Name "Allow Watch_France Binary frontend-streamer in" -DisplayName "Allow Watch_France Binary frontend-streamer in" -Action "Allow" -Direction "Inbound" -Protocol "tcp" -LocalPort 9090 -Program "C:\Watchmaker\application\Watch_France\Binaries\Win64\Watch_France.exe" -ErrorAction Ignore)){
+    Set-NetFirewallRule -Name "Allow Watch_France Binary frontend-streamer in" -Action "Allow" -Direction "Inbound" -Protocol "tcp" -LocalPort 9090 -Program "C:\Watchmaker\application\Watch_France\Binaries\Win64\Watch_France.exe"
+}
+
+
+if(!(New-NetFirewallRule -Name "Allow Watch_France frontend-streamer out" -DisplayName "Allow Watch_France frontend-streamer out" -Action "Allow" -Direction "Outbound" -Protocol "tcp" -LocalPort 9090 -Program "C:\Watchmaker\application\Watch_France.exe" -ErrorAction Ignore)){
+    Set-NetFirewallRule -Name "Allow Watch_France frontend-streamer out" -Action "Allow" -Direction "Outbound" -Protocol "tcp" -LocalPort 9090 -Program "C:\Watchmaker\application\Watch_France.exe"
+}
+if(!(New-NetFirewallRule -Name "Allow Watch_France Binary frontend-streamer out" -DisplayName "Allow Watch_France frontend-streamer out" -Action "Allow" -Direction "Outbound" -Protocol "tcp" -LocalPort 9090 -Program "C:\Watchmaker\application\Watch_France\Binaries\Win64\Watch_France.exe" -ErrorAction Ignore)){
+    Set-NetFirewallRule -Name "Allow Watch_France Binary frontend-streamer out" -Action "Allow" -Direction "Outbound" -Protocol "tcp" -LocalPort 9090 -Program "C:\Watchmaker\application\Watch_France\Binaries\Win64\Watch_France.exe"
 }
 
 
@@ -79,4 +79,12 @@ if(!(New-NetFirewallRule -Name "Allow Local Node ss-mm-streamer-communications i
 
 if(!(New-NetFirewallRule -Name "Allow Local Node ss-mm-streamer-communications out" -DisplayName "Allow Local Node ss-mm-streamer-communications out" -Action "Allow" -Direction "Outbound" -Protocol "tcp" -LocalPort 9090 -Program "C:\Watchmaker\pixelstreaming\platform_scripts\cmd\node\node.exe" -ErrorAction Ignore)){
     Set-NetFirewallRule -Name "Allow Local Node ss-mm-streamer-communications out" -Action "Allow" -Direction "Outbound" -Protocol "tcp" -LocalPort 9090 -Program "C:\Watchmaker\pixelstreaming\platform_scripts\cmd\node\node.exe"
+}
+
+if(!(New-NetFirewallRule -Name "Allow Local Node STUN/TURN in" -DisplayName "Allow Local Node STUN/TURN in" -Action "Allow" -Direction "Inbound" -Protocol "tcp" -LocalPort 19302,3478,5349  -Program "C:\Watchmaker\pixelstreaming\platform_scripts\cmd\node\node.exe" -ErrorAction Ignore)){
+    Set-NetFirewallRule -Name "Allow Local Node STUN/TURN in" -Action "Allow" -Direction "Inbound" -Protocol "tcp" -LocalPort 19302,3478,5349 -Program "C:\Watchmaker\pixelstreaming\platform_scripts\cmd\node\node.exe"
+}
+
+if(!(New-NetFirewallRule -Name "Allow Local Node STUN/TURN out" -DisplayName "Allow Local Node STUN/TURN out" -Action "Allow" -Direction "Outbound" -Protocol "tcp" -LocalPort 19302,3478,5349 -Program "C:\Watchmaker\pixelstreaming\platform_scripts\cmd\node\node.exe" -ErrorAction Ignore)){
+    Set-NetFirewallRule -Name "Allow Local Node STUN/TURN out" -Action "Allow" -Direction "Outbound" -Protocol "tcp" -LocalPort 19302,3478,5349 -Program "C:\Watchmaker\pixelstreaming\platform_scripts\cmd\node\node.exe"
 }
